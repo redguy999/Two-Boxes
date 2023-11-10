dc07b4ee468e47e95a08809c6fbfcc9db8214551
redguy999 <Bigredguy05@hotmail.com>
2023-11-10 15:10:27 -0500

    Adds second box

--- SquareR.gd
+++ SquareR.gd
@@ -7,7 +7,14 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
+func calculation():#calculates direction of movement.
+	#moved this to a different function cause its a bit of a mess
+	var hold = 0;
+	if(Input.is_key_pressed(KEY_A)):
+		hold=-1;
+	elif(Input.is_key_pressed(KEY_D)):
+		hold=1;
+	return hold;
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,7 +26,8 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	var direction = calculation()#Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
