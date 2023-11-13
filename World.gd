74957a3a19a1937a6778e2057780bc3be4378bfe
redguy999 <Bigredguy05@hotmail.com>
2023-11-13 09:14:18 -0500

    Adds a working transition to level 2

--- World.gd
+++ World.gd
@@ -9,3 +9,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+var simultaneous_scene = preload("level_2.tscn").instantiate()
+
+func nextLevel():
+	# This is like autoloading the scene, only
+	# it happens after already loading the main scene.
+	get_tree().root.add_child(simultaneous_scene)
+	get_tree().change_scene_to_file("level_2.tscn")
+	get_tree().change_scene_to_packed(simultaneous_scene)
+	#TODO: change this so it will cycle through the levels.
+func _on_area_2d_body_entered(body):
+	print("Test")
+	if(body.name=="SquareB"):
+		call_deferred("nextLevel")#load next level 
